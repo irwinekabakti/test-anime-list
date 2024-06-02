91736b78cc21cf958dc1f6d3dcfc13415e517efb
irwinekabakti <irwinekabakti@gmail.com>
2024-06-02 13:17:52 +0700

    add:(Navbar)

--- app/(shared)/Footer.tsx
+++ app/(shared)/Footer.tsx
@@ -5,13 +5,13 @@ const Footer: React.FC = () => {
   const year = date.getFullYear();
 
   return (
-    <div className="w-full h-20 bg-gray-200">
+    <footer className="w-full h-20 bg-gray-200">
       <div className="flex justify-center items-center h-full px-3">
         <p className="text-gray-600 text-sm">
           © {year} Irwin Ekabakti. All rights reserved
         </p>
       </div>
-    </div>
+    </footer>
   );
 };
 
---
+++ app/collections/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Collections: React.FC = () => {
+  return (
+    <div>
+      <h1>Collections</h1>
+    </div>
+  );
+};
+
+export default Collections;
