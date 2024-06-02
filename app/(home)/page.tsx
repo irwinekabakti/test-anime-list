1056adf15470fb53cf83ed4adb455f44373274a2
irwinekabakti <irwinekabakti@gmail.com>
2024-06-02 13:46:16 +0700

    add:(banner components)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Card from "../_components/Card";
+import Banner from "../_components/Banner";
 
 const Home: React.FC = () => {
   return (
     <div>
+      <Banner />
       <Card />
     </div>
   );
