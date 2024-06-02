dc2f020c3a9e6655812fed84a5fc173ba2662126
irwinekabakti <irwinekabakti@gmail.com>
2024-06-02 18:47:36 +0700

    fix:(CI/CD)

--- app/(home)/[detail]/page.tsx
+++ app/(home)/[detail]/page.tsx
@@ -1,29 +1,35 @@
 // pages/home/[detail]/[id].tsx
 
-import { useRouter } from "next/router";
-import { useQuery } from "@apollo/client";
-import { GET_ANIME_DETAILS } from "@/graphql/queries";
+// import { useRouter } from "next/router";
+// import { useQuery } from "@apollo/client";
+// import { GET_ANIME_DETAILS } from "@/graphql/queries";
 
 const DetailPage = () => {
-  const router = useRouter();
-  const { id } = router.query;
+  // const router = useRouter();
+  // const { id } = router.query;
 
-  const { loading, error, data } = useQuery(GET_ANIME_DETAILS, {
-    variables: { id },
-  });
+  // const { loading, error, data } = useQuery(GET_ANIME_DETAILS, {
+  //   variables: { id },
+  // });
 
-  console.log(data, "<==");
+  // console.log(data, "<==");
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  // if (loading) return <div>Loading...</div>;
+  // if (error) return <div>Error: {error.message}</div>;
 
-  const anime = data.Media;
+  // const anime = data.Media;
+
+  // return (
+  //   <div>
+  //     <h1>{anime.title.english || anime.title.romaji}</h1>
+  //     <img src={anime.coverImage.large} alt={anime.title.romaji} />
+  //     <p>{anime.description}</p>
+  //   </div>
+  // );
 
   return (
     <div>
-      <h1>{anime.title.english || anime.title.romaji}</h1>
-      <img src={anime.coverImage.large} alt={anime.title.romaji} />
-      <p>{anime.description}</p>
+      <h1>test</h1>
     </div>
   );
 };
